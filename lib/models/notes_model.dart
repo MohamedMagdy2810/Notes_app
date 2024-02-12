07b70879a29e1179a9f3951373d84d0cff259b90
Mohamed Magdy <mohamedgezar20@gmail.com>
2024-02-12 10:31:31 +0200

    genrate type adapter

--- lib/models/notes_model.dart
+++ lib/models/notes_model.dart
@@ -1,8 +1,17 @@
-class noteModel{
+import 'package:hive/hive.dart';
+
+part 'notes_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String supTitle;
+  @HiveField(2)
   final int color;
+  @HiveField(3)
   final String date;
 
-  noteModel({required this.title, required this.supTitle, required this.color, required this.date});
+  NoteModel({required this.title, required this.supTitle, required this.color, required this.date});
 }
